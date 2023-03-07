d5d855d481321ab589ada98589eae9126ff41b23
Guseva <tyurinaekaterina@yandex.ru>
2023-03-07 10:51:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int price;
+        price = 15698;
+        int quantity;
+        quantity = 20;
+
+     System.out.println(" количество бонусных миль " + (price / quantity));
+    }
+
+
+}
